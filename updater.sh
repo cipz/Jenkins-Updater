7029e193e1f2cdc70426c0894363041971fe8a78
Ciprian Voinea <34091723+cipz@users.noreply.github.com>
2020-01-24 10:45:53 +0100

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 
+echo "Stopping the Jenkins instance"
+sudo service jenkins stop;
+
+echo "Creating jenkins.war.old";
+sudo mv jenkins.war jenkins.war.old;
+
+echo "Downloading new installation file"
+sudo wget https://updates.jenkins-ci.org/latest/jenkins.war;
+
+echo "Restarting Jenkins"
+sudo service jenkins start;
